04fec17a60426ce85573b1039fa254f7369446d2
CassieW999 <cassie.wenlh@gmail.com>
2020-08-29 16:56:32 +0800

    link list

---
+++ src/ReverseLinkedListII.java
@@ -0,0 +1,34 @@
+public class ReverseLinkedListII {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode cur = head, prev = null;
+        while (m > 1) {
+            prev = cur;
+            cur = cur.next;
+            m--;
+            n--;
+        }
+
+        ListNode con = prev, tail = cur;
+
+        ListNode third = null;
+        while (n > 0) {
+            third = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = third;
+            n--;
+        }
+
+        if (con != null) {
+            con.next = prev;
+        } else {
+            head = prev;
+        }
+
+        tail.next = cur;
+        return head;
+    }
+}
