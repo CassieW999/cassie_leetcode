eacd85e3a78888f464cff07ab209f2f0a4f07ce9
CassieW999 <cassie.wenlh@gmail.com>
2020-09-20 09:19:20 +0800

    Binary Tree

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,15 @@
+import javax.swing.tree.TreeNode;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        return root;
+    }
+}
