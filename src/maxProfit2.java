ed7af266ebf5c900fb28cd669765c3b6e89e1cf4
CassieW999 <cassie.wenlh@gmail.com>
2020-09-28 23:10:04 +0800

    maxProfit2

---
+++ src/maxProfit2.java
@@ -0,0 +1,13 @@
+public class maxProfit2 {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1) return 0;
+        int res = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]){
+                res += prices[i] - prices[i - 1];
+            }
+        }
+
+        return res;
+    }
+}
