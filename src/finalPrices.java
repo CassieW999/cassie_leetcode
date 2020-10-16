b121bb7f82ff73b0fab9d026dd7a52d737e21030
CassieW999 <cassie.wenlh@gmail.com>
2020-10-16 20:53:53 +0800

    easy + stack

--- src/finalPrices.java
+++ src/finalPrices.java
@@ -1,13 +1,26 @@
+import java.util.*;
+
 public class finalPrices {
     public int[] finalPrices(int[] prices) {
-        int n = prices.length;
-        for (int i = 0; i < n; i++){
-            for (int j = i + 1; j < n; j ++){
-                if (prices[j] <= prices[i]){
-                    prices[i] = prices[i] - prices[j];
-                    break;
-                }
-            }
+        //////////     solution 1      //////////
+//        int n = prices.length;
+//        for (int i = 0; i < n; i++){
+//            for (int j = i + 1; j < n; j ++){
+//                if (prices[j] <= prices[i]){
+//                    prices[i] = prices[i] - prices[j];
+//                    break;
+//                }
+//            }
+//        }
+//        return prices;
+
+
+        //////////     solution 2     //////////
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i=0; i<prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i])
+                prices[stack.pop()] -= prices[i];
+            stack.push(i);
         }
         return prices;
     }
