b4fc0cb1356ecf2a4e331cc047b1baf3cd73d320
CassieW999 <cassie.wenlh@gmail.com>
2020-11-08 16:50:12 +0800

    binary search

---
+++ src/singleNonDuplicate.java
@@ -0,0 +1,15 @@
+
+
+public class singleNonDuplicate {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r){
+            int m = l + (r - l)/2;
+            int n = m % 2 == 0 ? m + 1: m - 1;
+            if (nums[m] == nums[n]) l = m + 1;
+            else r = m;
+        }
+        return nums[l];
+    }
+}
