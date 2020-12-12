7f77f361c1163726315d75ef66566cf78a2c9186
CassieW999 <cassie.wenlh@gmail.com>
2020-12-12 22:31:10 +0800

    BINARY SEARCH(HARD)

--- src/findMedianSortedArrays.java
+++ src/findMedianSortedArrays.java
@@ -35,4 +35,43 @@ public class findMedianSortedArrays {
             return mergedNum[(m+n)/2];
         }
     }
+
+
+                          ////////////////       解题思路        ///////////////////
+    //这种思路就是最直观的扫描合并然后取中间值，虽然通过了但是并没有符合follow up的时间复杂度要求
+    //时间复杂度的要求为log(m+n)，显然是需要我们采取二分
+    public double findMedianSortedArrays2(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) { // to ensure m<=n
+            int[] temp = A; A = B; B = temp;
+            int tmp = m; m = n; n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j-1] > A[i]){
+                iMin = i + 1; // i is too small
+            }
+            else if (i > iMin && A[i-1] > B[j]) {
+                iMax = i - 1; // i is too big
+            }
+            else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
 }
