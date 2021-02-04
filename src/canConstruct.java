725f1267b769483a0484af4b8fea629e327985bb
CassieW999 <cassie.wenlh@gmail.com>
2021-02-04 16:27:54 +0800

    easy string

---
+++ src/canConstruct.java
@@ -0,0 +1,14 @@
+public class canConstruct {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            letters[c - 'a'] ++;
+        }
+
+        for (char c : ransomNote.toCharArray()){
+            if (letters[c - 'a'] -- <= 0) return false;
+        }
+        return true;
+    }
+}
