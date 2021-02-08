5c27b0397997e12f4719b9aac9459e93885185bc
CassieW999 <cassie.wenlh@gmail.com>
2021-02-08 16:54:18 +0800

    dp + 二维 [mediums]

--- src/numberOfArithmeticSlices.java
+++ src/numberOfArithmeticSlices.java
@@ -8,8 +8,6 @@ public class numberOfArithmeticSlices {
         int n = A.length;
         if (n<3)return 0;
         int[] dp = new int[n];
-        dp[0] = 0;
-        dp[1] = 0;
         for (int i = 1; i < n-1; i++) {
             if (A[i+1] - A[i] == A[i] - A[i-1]){
                 dp[i+1] = dp[i] + 1;
