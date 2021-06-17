669b4f052c9b8122834676bc1f3f028531ab3fcc
CassieW999 <cassie.wenlh@gmail.com>
2021-06-17 16:44:58 +0800

    random

--- src/canJump.java
+++ src/canJump.java
@@ -1,11 +1,24 @@
-public class canJump {
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int rightMostPos = 0;
-        for (int i = 0; i <= rightMostPos; i++) {
-            rightMostPos = Math.max(rightMostPos, nums[i] + i);
-            if (rightMostPos >= n - 1) return true;
-        }
-        return false;
-    }
-}
+//public class canJump {
+//    public boolean canJump(int[] nums) {
+//        int n = nums.length;
+//        int rightMostPos = 0;
+//        for (int i = 0; i <= rightMostPos; i++) {
+//            rightMostPos = Math.max(rightMostPos, nums[i] + i);
+//            if (rightMostPos >= n - 1) return true;
+//        }
+//        return false;
+//    }
+//
+//    public TreeNode buildTree(int[] preorder, int[] inorder) {
+//        if (preorder.length != inorder.length || preorder.length == 0) return null;
+//
+//        int headValue = preorder[0];
+//        TreeNode head = new TreeNode(headValue);
+//        int headIndex = findIndex(inorder, headValue);
+//        TreeNode left_sub = buildTree(preorder.);
+//        TreeNode right_sub = buildTree();å
+//        head.left = left_sub;
+//        head.right = right_sub;
+//        return head;
+//    }
+//}
